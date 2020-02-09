1d83a44e79cb4eff39e50bbfb059ec9f0a351f3f
CinquinAndy <cinquin.andy@gmail.com>
2020-02-09 13:46:02 +0100

    Ajouts fonctionnalités : -> Bouton accueil, pour revenir au début -> Ajout liens linkedin / Cv -> Ajout des tous les éléments cliquable, tel, email , et différents liens Ajout du copyright
    Changement de la dispositions des éléments avec arrow down et Developped by Cinquin Andy
    
    Ajout d'animation au changement de page
    Slide in / Slide Out , anciennement uniquement Slide In
    
    Et quelques corrections de css / changement mineurs

--- fonction.js
+++ fonction.js
@@ -24,53 +24,141 @@ function changementtaillefenetre(){
 
 function clickSuite1(){
     // click sur accueil 1->2
+    Page1.style.animationName='slideout';
+    Page1.style.marginTop="100vh";
+    setTimeout(clickSuite1_AfterAnimation,1000);
+}
+function clickSuite1_AfterAnimation() {
     Page1.style.display="none";
+    Page1.style.animationName='slidein';
+    Page1.style.marginTop="0";
     Page2.style.display="flex";
     stop();
 }
+
 function clickSuite2(){
     // click sur qui suis-je ? -> go sur mes projets -> 2->5
+    Page2.style.animationName='slideout';
+    Page2.style.marginTop="100vh";
+    setTimeout(clickSuite2_AfterAnimation,1000);
+}
+function clickSuite2_AfterAnimation(){
+    // click sur qui suis-je ? -> go sur mes projets -> 2->5
     Page2.style.display="none";
+    Page2.style.animationName='slidein';
+    Page2.style.marginTop="0";
     Page5.style.display="flex";
 }
+
 function clickSuite2More(){
     // click pour en savoirs plus -> go sur mon parcours -> 2->3
+    Page2.style.animationName='slideout';
+    Page2.style.marginTop="100vh";
+    setTimeout(clickSuite2More_AfterAnimation,1000);
+}
+function clickSuite2More_AfterAnimation(){
+    // click pour en savoirs plus -> go sur mon parcours -> 2->3
     Page2.style.display="none";
+    Page2.style.animationName='slidein';
+    Page2.style.marginTop="0";
     Page3.style.display="flex";
 }
+
 function clickSuite3(){
     // click sur mon parcours -> go sur mes projets 3->5
+    Page3.style.animationName='slideout';
+    Page3.style.marginTop="100vh";
+    setTimeout(clickSuite3_AfterAnimation,1000);
+}
+function clickSuite3_AfterAnimation(){
+    // click sur mon parcours -> go sur mes projets 3->5
     Page3.style.display="none";
+    Page3.style.animationName='slidein';
+    Page3.style.marginTop="0";
     Page5.style.display="flex";
 }
+
 function clickSuite3More(){
     // click pour en savoirs plus -> go sur mon histoire, passions -> 3->4
+    Page3.style.animationName='slideout';
+    Page3.style.marginTop="100vh";
+    setTimeout(clickSuite3More_AfterAnimation,1000);
+}
+function clickSuite3More_AfterAnimation(){
+    // click pour en savoirs plus -> go sur mon histoire, passions -> 3->4
     Page3.style.display="none";
+    Page3.style.animationName='slidein';
+    Page3.style.marginTop="0";
     Page4.style.display="flex";
 }
+
 function clickSuite4(){
     // click sur Mon histoire, Mes passions -> go sur mes projets 4->5
+    Page4.style.animationName='slideout';
+    Page4.style.marginTop="100vh";
+    setTimeout(clickSuite4_AfterAnimation,1000);
+}
+function clickSuite4_AfterAnimation(){
+    // click sur Mon histoire, Mes passions -> go sur mes projets 4->5
     Page4.style.display="none";
+    Page4.style.animationName='slidein';
+    Page4.style.marginTop="0";
     Page5.style.display="flex";
 }
+
 function clickSuite5(){
     // click sur mes projets/réalisations -> go sur mes expériences 5->6
+    Page5.style.animationName='slideout';
+    Page5.style.marginTop="100vh";
+    setTimeout(clickSuite5_AfterAnimation,1000);
+}
+function clickSuite5_AfterAnimation(){
+    // click sur mes projets/réalisations -> go sur mes expériences 5->6
     Page5.style.display="none";
+    Page5.style.animationName='slidein';
+    Page5.style.marginTop="0";
     Page6.style.display="flex";
 }
+
 function clickSuite6(){
     // click sur Expériences -> go sur mes compétences 6->7
+    Page6.style.animationName='slideout';
+    Page6.style.marginTop="100vh";
+    setTimeout(clickSuite6_AfterAnimation,1000);
+}
+function clickSuite6_AfterAnimation(){
+    // click sur Expériences -> go sur mes compétences 6->7
     Page6.style.display="none";
+    Page6.style.animationName='slidein';
+    Page6.style.marginTop="0";
     Page7.style.display="flex";
 }
+
 function clickSuite7(){
     // click sur mes compétences -> go sur les contacts 7->8
+    Page7.style.animationName='slideout';
+    Page7.style.marginTop="100vh";
+    setTimeout(clickSuite7_AfterAnimation,1000);
+}
+function clickSuite7_AfterAnimation(){
+    // click sur mes compétences -> go sur les contacts 7->8
     Page7.style.display="none";
+    Page7.style.animationName='slidein';
+    Page7.style.marginTop="0";
     Page8.style.display="flex";
 }
+
 function clickSuite8(){
     // click sur contact - retour accueil 8->1
+    Page8.style.animationName='slideout';
+    Page8.style.marginTop="100vh";
+    setTimeout(clickSuite8_AfterAnimation,1000);
+}
+function clickSuite8_AfterAnimation(){
+    // click sur contact - retour accueil 8->1
     Page8.style.display="none";
+    Page8.style.animationName='slidein';
+    Page8.style.marginTop="0";
     Page1.style.display="flex";
     interval=setInterval(change, 30);
     interval2=setInterval(Rchange, 30);
