66e33d30ffa9c1defda15c07152ecafd67a3a7fa
Andy <cinquin.andy@gmail.com>
2020-07-06 23:32:16 +0200

    v2 website, Bidouillage couleur, mise en place d'hover, Adaptation graphisme en 680pixels+ et en 1440+

--- fonction.js
+++ fonction.js
@@ -5,6 +5,10 @@ function changementtaillefenetre(){
     let quatriemeImage = document.getElementById("quatriemeImageCarroussel");
     let cinquiemeImage = document.getElementById("cinquiemeImageCarroussel");
     let sixiemeImage = document.getElementById("sixiemeImageCarroussel");
+    let septiemeImage = document.getElementById("septiemeImageCarroussel");
+    let huitiemeImage = document.getElementById("huitiemeImageCarroussel");
+    let neuviemeImage = document.getElementById("neuviemeImageCarroussel");
+    let dixiemeImage = document.getElementById("dixiemeImageCarroussel");
     if (document.body.clientWidth>=680){
         premiereImage.src='images/cinquinMaeva.png';
         deuxiemeImage.src='images/Conseils.png';
@@ -12,6 +16,10 @@ function changementtaillefenetre(){
         quatriemeImage.src='images/CinquinAndy.png';
         cinquiemeImage.src='images/BewSite.png';
         sixiemeImage.src='images/Endar.png';
+        septiemeImage.src='images/Hublot.png';
+        huitiemeImage.src='images/LogoPerso.png';
+        neuviemeImage.src='images/PortefolioV2.png';
+        dixiemeImage.src='images/ProjetRetroAliceDesktop.png';
     }else if (document.body.clientWidth<680){
         premiereImage.src='images/cinquinMaevaPhone.png';
         deuxiemeImage.src='images/ConseilsPhone.png';
@@ -19,5 +27,9 @@ function changementtaillefenetre(){
         quatriemeImage.src='images/CinquinAndyPhone.png';
         cinquiemeImage.src='images/BewSitePhone.png';
         sixiemeImage.src='images/EndarPhone.png';
+        septiemeImage.src='images/HublotPhone.png';
+        huitiemeImage.src='images/LogoPersoPhone.png';
+        neuviemeImage.src='images/PortefolioV2Phone.png';
+        dixiemeImage.src='images/ProjetRetroAlicePhone.png';
     }
 }
